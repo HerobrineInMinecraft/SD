03bd3367f463cf46d892539abdc508342dec1ac4
Firefly <WUXIANJITUANCEO@163.com>
2026-05-19 14:56:13 +0800

    重构

--- src/main/java/com/sd/Main.java
+++ src/main/java/com/sd/Main.java
@@ -1,35 +1,5 @@
 package com.sd;
 
-import com.sd.core.Interpreter;
-import com.sd.core.SdLexer;
-import com.sd.core.SdParser;
-import com.sd.core.Token;
-import com.sd.core.ast.AstNode;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
 
-        String code = """
-let volatile a = 1;
-let synchronized b = 2;
-let sync c = 3;
-let send d = 4;
-let unsafe e = 5;
-            """;
-
-        SdLexer lexer = new SdLexer(code);
-        List<Token> tokens = lexer.scan();
-
-        SdParser parser = new SdParser(tokens);
-        List<AstNode> program = parser.parseProgram();
-
-        Interpreter interpreter = new Interpreter();
-        System.out.println("========== SD 语言运行成功 ==========");
-        for (AstNode node : program) {
-            interpreter.run(node);
-        }
-    }
 }
---
+++ src/main/java/com/sd/lexer/Lexer.java
@@ -0,0 +1,4 @@
+package com.sd.lexer;
+
+public class Lexer {
+}
